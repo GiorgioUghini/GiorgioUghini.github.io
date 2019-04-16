f78368dc1b44b62621112be15d2849b11626fb0c
GiorgioUghini <giorgio1937@hotmail.it>
2019-04-16 19:25:02 +0200

    Update site

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 git add --all
 git commit -m "Update site"
 git push
-sleep 3
-git commit -m 'rebuild pages' --allow-empty
-git push
-sleep 2
+sleep 5
 git commit -m 'rebuild pages' --allow-empty
 git push
